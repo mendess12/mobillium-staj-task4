81755d7a090e9f0b75eaa1314340f469dbc59c9a
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-05-15 16:32:10 +0300

    Add tool bar onclick method

--- app/src/main/java/com/example/task4/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/task4/ui/detail/DetailFragment.kt
@@ -5,14 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.task4.R
 import com.example.task4.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
-    private lateinit var binding : FragmentDetailBinding
-    private val viewModel : DetailViewModel by viewModels()
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +23,9 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailBinding.bind(view)
+
+        binding.detailScreenToolBar.backToolBar.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
