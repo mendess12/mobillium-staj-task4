280e83f5efc91a829b9ffed97a78137589287138
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-05-15 16:28:13 +0300

    Add response api in view model

--- app/src/main/java/com/example/task4/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/task4/ui/list/ListFragment.kt
@@ -1,18 +1,25 @@
 package com.example.task4.ui.list
 
+import CryptoAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.task4.R
 import com.example.task4.databinding.FragmentListBinding
+import com.example.task4.model.CryptoListItem
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), CryptoAdapter.Listener {
 
     private lateinit var binding: FragmentListBinding
     private val viewModel: ListViewModel by viewModels()
+    private lateinit var dataAdapter: CryptoAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +31,27 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentListBinding.bind(view)
+
+        binding.recyclerView.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = linearLayoutManager
+        observeLiveData()
+        viewModel.getCryptoDataFromAPI()
+    }
+
+    private fun observeLiveData() {
+        viewModel.cryptoDataList.observe(viewLifecycleOwner) {
+            if (it != null) {
+                dataAdapter = CryptoAdapter(it, this@ListFragment)
+                binding.recyclerView.adapter = dataAdapter
+            } else {
+                Toast.makeText(requireContext(), "List is empty!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onItemClick(cryptoData: CryptoListItem) {
+        val action = ListFragmentDirections.actionListFragmentToDetailFragment()
+        findNavController().navigate(action)
     }
 }
--- app/src/main/java/com/example/task4/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/task4/ui/list/ListViewModel.kt
@@ -1,6 +1,41 @@
 package com.example.task4.ui.list
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.task4.di.RetrofitModule
+import com.example.task4.model.CryptoListItem
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListViewModel : ViewModel() {
+
+    private val retrofitModule = RetrofitModule()
+    val cryptoDataList = MutableLiveData<List<CryptoListItem>?>()
+
+    fun getCryptoDataFromAPI() {
+        viewModelScope.launch {
+            retrofitModule.getListData().enqueue(
+                object : Callback<List<CryptoListItem>> {
+                    override fun onResponse(
+                        call: Call<List<CryptoListItem>>,
+                        response: Response<List<CryptoListItem>>
+                    ) {
+                        if (response.isSuccessful) cryptoDataList.value =
+                            response.body() else cryptoDataList.value = null
+                    }
+
+                    override fun onFailure(call: Call<List<CryptoListItem>>, t: Throwable) {
+                        cryptoDataList.value = null
+                        Log.d("Error message", t.printStackTrace().toString())
+                    }
+
+                })
+
+        }
+    }
+
 }
