89b76c3e03d438c856af6ef8fd1f2362bcab9d5e
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-05-18 10:08:55 +0300

    Show crypto data

--- app/src/main/java/com/example/task4/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/task4/ui/detail/DetailFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.task4.R
 import com.example.task4.databinding.FragmentDetailBinding
+import com.example.task4.util.downloadFromUrl
 
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +27,27 @@ class DetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailBinding.bind(view)
 
+        printCryptoData()
         binding.detailScreenToolBar.backToolBar.setOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun printCryptoData() {
+        val cryptoDataArgs = args.crypto
+        cryptoDataArgs.let {
+            binding.apply {
+                detailScreenIv.downloadFromUrl(cryptoDataArgs.image)
+                detailScreenNameTv.text = cryptoDataArgs.name
+                detailScreenYearTv.text = cryptoDataArgs.yearEstablished.toString()
+                detailScreenCountryTv.text = cryptoDataArgs.country
+                detailScreenDescriptionTv.text = cryptoDataArgs.description
+                detailScreenTrustScoreTv.text = cryptoDataArgs.trustScore.toString()
+                detailScreenTrustScoreRankTv.text = cryptoDataArgs.trustScoreRank.toString()
+                detailScreenTradeVolume24hBtcTv.text = cryptoDataArgs.tradeVolume.toString()
+                detailScreenTradeVolume24hBtcNormalizedTv.text =
+                    cryptoDataArgs.tradeVolumeNormalized.toString()
+            }
         }
     }
 }
